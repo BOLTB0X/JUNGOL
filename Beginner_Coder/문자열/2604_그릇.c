6d6f5509571180169f93493a6dfc9f4f211cd697
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-13 14:32:22 +0900

    그릇
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1865&sca=2050

---
+++ Beginner_Coder/문자열/2604_그릇.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char str[101];
+	int result = 10;
+
+	scanf("%s", str);
+	for (int i = 1; i < strlen(str); ++i) {
+		if (str[i - 1] == str[i])
+			result += 5;
+		else
+			result += 10;
+	}
+
+	printf("%d\n", result);
+	return 0;
+}
