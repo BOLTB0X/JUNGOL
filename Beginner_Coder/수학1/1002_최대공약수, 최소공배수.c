7b4ede2b913d2199f1b239117456690d4c92635a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-19 13:57:59 +0900

    최대공약수, 최소공배수
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=281&sca=2030

---
+++ Beginner_Coder/수학1/1002_최대공약수, 최소공배수.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//최대공약수
+int gcd(int a, int b) {
+	if (a % b == 0)
+		return b;
+	else
+		return gcd(b, a % b);
+}
+
+//최소공배수
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
+}
+
+void solution(void) {
+	int n;
+	int arr[10];
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; ++i)
+		scanf("%d", &arr[i]);
+
+	int ret1 = arr[0];
+	int ret2 = arr[0];
+
+	for (int i = 1; i < n; ++i) {
+		ret1 = gcd(ret1, arr[i]);
+		ret2 = lcm(ret2, arr[i]);
+	}
+
+	printf("%d %d\n", ret1, ret2);
+	return;
+}
+
+int main(void) {
+	solution();
+	return 0;
+}
