90f1df187f252ded9ead21f56d0bb8a780058c05
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-28 13:43:41 +0900

    스택 (stack)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=382&sca=2070

---
+++ Beginner_Coder/자료처리/1102_스택 (stack)_Adt.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int stack[100];
+int top = -1;
+
+int is_Full(void) {
+	//가득 차있는지
+	if (top >= 99)
+		return 1;
+	return 0;
+}
+
+int is_Empty(void) {
+	if (top == -1) 
+		return 1;
+	
+	return 0;
+}
+
+void push(int data) {
+	if (!is_Full()) 
+		stack[++top] = data;
+	return;
+}
+
+int pop(void) {
+	if (!is_Empty()) {
+		int tmp = stack[top--];
+		return tmp;
+	}
+	return -1;
+}
+
+void solution(void) {
+	int n;
+	char cmd;
+	int data;
+	scanf("%d", &n);
+
+	for (int j=0; j< n; ++j) {
+		scanf(" %c", &cmd);
+
+		if (cmd == 'i') {
+			scanf(" %d", &data);
+			push(data);
+		}
+
+		else if (cmd == 'o') {
+			if (is_Empty())
+				printf("empty\n");
+			else
+				printf("%d\n", pop());
+		}
+
+		else if (cmd == 'c')
+			printf("%d\n", top + 1);
+	}
+
+	return;
+}
+
+int main(void) {
+	solution();
+	return 0;
+}
