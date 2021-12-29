0a65e5a080e2a65ea2eb892b60c0fbcc792d0719
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-29 14:58:10 +0900

    문자마름모
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=2073&sca=2020

--- Beginner_Coder/도형만들기2/1331_문자마름모.c
+++ Beginner_Coder/도형만들기2/1331_문자마름모.c
@@ -2,40 +2,42 @@
 #include <stdio.h>
 
 char pane[210][210];
-const int dy[5] = { 0,1,1,-1,-1 };
-const int dx[5] = { 0,-1,1,1,-1 };
-
+const int dy[4] = { 1,1,-1,-1 };
+const int dx[4] = { -1,1,1,-1 };
 
 void fill_shape(int n) {	
 	int len = n;
 	char alp = 'A';
 
-	for (int i = 1; i <= n; ++i) {
+	for (int i = 1; i < n; ++i) {
 		int cy = i, cx = n;
-		for (int dir = 1; dir <= 4; ++dir) {
+		for (int dir = 0; dir < 4; ++dir) {
 			for (int j = 1; j < len; ++j) {
 				pane[cy][cx] = alp++;
+
 				if (alp > 'Z')
 					alp = 'A';
-				cy = cy + dy[dir];
-				cx = cx + dx[dir];
+
+				cy += dy[dir];
+				cx += dx[dir];
 			}
 		}
 		len--;
 	}
 
 	pane[n][n] = alp;
-	
 	return;
 }
 
 void print_shape(int n) {
 	int left = n, right = n;
+
 	for (int i = 1; i <= 2 * n - 1; ++i) {
-		for (int j = 1; j < left; ++j) 
+		for (int j = 1; j < left; ++j)
 			printf("  ");
 		for (int j = left; j <= right; ++j)
 			printf("%c ", pane[i][j]);
+
 		if (i < n)
 			left--, right++;
 		else
