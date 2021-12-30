8f0612eccc5e0791a4b171b2ba394ed4224737d8
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-12-30 17:12:40 +0900

    진법 변환
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=2375&sca=2040

--- Beginner_Coder/수학2/3106_진법 변환.c
+++ Beginner_Coder/수학2/3106_진법 변환.c
@@ -2,40 +2,54 @@
 #include <stdio.h>
 #include <string.h>
 
-int a, b;
-char str[1000001];
+long long a_to_dec(int a, char* str) {
+	long long tot = 0;
+	long long i, j;
+	for (i = strlen(str) - 1, j = 1; i >= 0; --i, j *= a) {
+		if (str[i] >= 'A')
+			str[i] = '9' + (str[i] - 'A') + 1;
+		tot += j * (str[i] - '0');
+	}
+
+	return tot;
+}
+
+void dec_to_b(int b, long long dec) {
+	char res[100];
+	long long i;
+	for (i = 0; dec > 0; dec /= b, ++i) {
+		if (dec % b > 9)
+			res[i] = (dec % b) - 10 + 'A';
+		else
+			res[i] = (dec % b) + '0';
+	}
+
+	for (i = i - 1; i >= 0; --i)
+		printf("%c", res[i]);
+	printf("\n");
+
+	return;
+}
 
 void solution(void) {
+	int a, b;
+	char str[1000001];
+
 	while (1) {
-		scanf("%d %s %d", &a, str, &b);
-		if (a == 0) break;
-	
+		scanf("%d", &a);
+
+		if (a == 0)
+			break;
+
+		scanf("%s %d", str, &b);
+
 		if (str[0] == '0') {
 			printf("0\n");
 			continue;
 		}
 
-		long long i, j;
-		long long tot = 0;
-		for (i = strlen(str) - 1, j = 1; i >= 0; i--, j *= a) {
-			if (str[i] >= 'A')
-				str[i] = '9' + (str[i] - 'A') + 1;
-			tot += j * (str[i] - '0');
-		}
-
-		char res[100];
-
-		for (i = 0; tot > 0; tot /= b, ++i) {
-			int tmp = tot % b;
-			if (tmp > 9)
-				res[i] = tmp - 10 + 'A';
-			else
-				res[i] = tmp + '0';
-		}
-
-		for (i = i - 1; i >= 0; --i)
-			printf("%c", res[i]);
-		printf("\n");
+		//a진법을 10진법으로 한 뒤 10진법을 b진법으로
+		dec_to_b(b,a_to_dec(a, str));
 	}
 
 	return;
