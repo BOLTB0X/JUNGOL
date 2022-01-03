95fc870c11dc8fe0d50f76fe2cdb92f967376418
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-03 13:58:25 +0900

    기초문제

--- Language_Coder/함수3/590_함수3 - 자가진단5.c
+++ Language_Coder/함수3/590_함수3 - 자가진단5.c
@@ -1,30 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int n;
 int per[101];
 
-void DFS(int n, int level) {
+void dice(int level) {
 	if (level > n) {
 		for (int i = 1; i <= n; ++i) 
 			printf("%d ", per[i]);
 		printf("\n");
 		return;
 	}
-
+	
 	for (int i = per[level - 1]; i <= 6; ++i) {
 		per[level] = i;
-		DFS(n, level + 1);
+		dice(level + 1);
 	}
 
 	return;
 }
 
 void solution(void) {
-	int n;
 	scanf("%d", &n);
 
 	per[0] = 1;
-	DFS(n, 1);
+
+	dice(1);
 	return;
 }
 
