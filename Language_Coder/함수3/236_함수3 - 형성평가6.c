f0603103519b8755421db4f052f45dfd94a576b9
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-03 15:20:01 +0900

    기초문제

--- Language_Coder/함수3/236_함수3 - 형성평가6.c
+++ Language_Coder/함수3/236_함수3 - 형성평가6.c
@@ -5,9 +5,9 @@ int recur(int n) {
 	if (n == 0)
 		return 1;
 
-	if (n % 10 == 0)
+	else if (n % 10 == 0)
 		return 1 * recur(n / 10);
-	else
+	else if (n % 10 != 0)
 		return n % 10 * recur(n / 10);
 
 }
