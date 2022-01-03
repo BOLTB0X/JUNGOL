904216b86f9329d72fdebc36111ebfe3a2e93d46
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-03 13:10:28 +0900

    기초문제

--- Language_Coder/문자열1/187_문자열1 - 형성평가6.c
+++ Language_Coder/문자열1/187_문자열1 - 형성평가6.c
@@ -1,31 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(void) {
+//substr 정의
+char* substr(char* str, int start_idx, int cnt) {
+	char* tmp = malloc(sizeof(char) * cnt + 1);
+
+	strncpy(tmp, str + start_idx, cnt);
+	tmp[cnt] = '\0';
+	return tmp;
+}
+
+void solution(void) {
 	char str[101] = { '\0', };
+	int idx;
+
 	scanf("%s", str);
-	int len = strlen(str);
 
 	while (strlen(str) > 1) {
-		char tmp[101] = { "\0", };
-		int idx;
+		char* tmp = malloc(sizeof(char) * strlen(str) + 1);
+		
 		scanf("%d", &idx);
 
-		if (idx > strlen(str))
-			str[strlen(str) - 1] = '\0';
-		else {
-			for (int i = 0; i < len; ++i) {
-				if (i == idx - 1)
-					continue;
-				tmp[i] = str[i];
-			}
+		if (idx > strlen(str)) {
+			tmp = substr(str, 0, strlen(str) - 1);
+			strcpy(str, tmp);
+
+			printf("%s\n", str);
 		}
 
-		strcpy(str, tmp);
-
-		printf("%s\n", str);
+		else {
+			tmp = substr(str, 0, idx - 1);
+			strcat(tmp, substr(str, idx, strlen(str)));
+			strcpy(str, tmp);
+			printf("%s\n", str);
+		}
 	}
+}
+
+int main(void) {
+	solution();
 
 	return 0;
 }
