51f353a3d7da871d941aa2d60dfe7fb97dd798a4
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-04 22:45:52 +0900

    떡 먹는 호랑이
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1270&sca=2060

---
+++ Beginner_Coder/여러가지/1997_떡 먹는 호랑이.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int days[31] = { 0, };
+int flag = 0;
+
+void solution(void) {
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	for (int i = 1; i <= k/2; ++i) {
+		for (int j = i + 1; j <= k; ++j) {
+			days[1] = i;
+			days[2] = j;
+			
+			for (int l = 3; l <= n; ++l) {
+				days[l] = days[l - 1] + days[l - 2];
+				
+				if (days[l] >= k)
+					break;
+			}
+
+			if (days[n] == k) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+	printf("%d\n", days[1]);
+	printf("%d\n", days[2]);
+
+	return;
+}
+
+int main(void) {
+	solution();
+	return 0;
+}
