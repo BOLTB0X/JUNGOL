8a550fea8924841ee77c0e52b499d9f8f05b710f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-10 14:44:09 +0900

    진법변환
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=2375&sca=2040

--- Beginner_Coder/수학2/3106_진법 변환.c
+++ Beginner_Coder/수학2/3106_진법 변환.c
@@ -2,68 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-void print_answer(char* str) {
-	for (int i = strlen(str) - 1; i >= 0; --i)
-		printf("%c", str[i]);
-	printf("\n");
-
-	return;
-}
-*/
-
-long long a_to_dec(int a, char* str) {
+long long trans_num(int type, char* n) {
 	long long tot = 0;
+
 	long long i, j;
-	for (i = strlen(str) - 1, j = 1; i >= 0; --i, j *= a) {
-		if (str[i] >= 'A')
-			str[i] = '9' + (str[i] - 'A') + 1;
-		tot += j * (str[i] - '0');
+	for (i = strlen(n) - 1, j = 1; i >= 0; --i, j *= type) {
+		if (n[i] >= 'A') 
+			n[i] = '9' + (n[i] - 'A') + 1;
+		tot += j * (n[i] - '0');
 	}
 
 	return tot;
 }
 
-void dec_to_b(int b, long long dec) {
-	char res[100];
-	long long i;
-	for (i = 0; dec > 0; dec /= b, ++i) {
-		if (dec % b > 9)
-			res[i] = (dec % b) - 10 + 'A';
-		else
-			res[i] = (dec % b) + '0';
-	}
-	//만약 정답 출력을 위해 strlen을 쓸 거면
-	// res[i] = '\0'; 마지막에 널을 추가
-	for (i = i - 1; i >= 0; --i)
-		printf("%c", res[i]);
-	printf("\n");
+void trans_type2(int type, long long n) {
+	char num[1001];
 
+	long long idx = 0;
+	while (n > 0) {
+		if (n % type > 9)
+			num[idx++] = (n % type) - 10 + 'A';
+		else
+			num[idx++] = (n % type) + '0';
+		n /= type;
+	}
+	num[idx] = '\0';
+
+	for (long long i = strlen(num) - 1; i >= 0; --i) {
+		printf("%c", num[i]);
+	}
+	printf("\n");
 	return;
 }
 
 void solution(void) {
-	int a, b;
-	char str[1000001];
+	int type1, type2;
+	char num[1000001];
 
 	while (1) {
-		scanf("%d", &a);
+		scanf("%d", &type1);
 
-		if (a == 0)
+		if (type1 == 0)
 			break;
 
-		scanf("%s %d", str, &b);
+		scanf("%s %d", num, &type2);
 
-		if (str[0] == '0') {
+		if (num[0] == '0') {
 			printf("0\n");
 			continue;
 		}
 
-		//a진법을 10진법으로 한 뒤 10진법을 b진법으로
-		dec_to_b(b,a_to_dec(a, str));
+		trans_type2(type2, trans_num(type1, num));
 	}
-
-	return;
 }
 
 int main(void) {
