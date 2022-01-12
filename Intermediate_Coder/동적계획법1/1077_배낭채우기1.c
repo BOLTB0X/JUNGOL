21a6d69dd0cbe2217d06048e10db1d88bf6c273d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-01-12 14:37:04 +0900

    배낭채우기1
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=357&sca=3060

---
+++ Intermediate_Coder/동적계획법1/1077_배낭채우기1.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int dp[10001];
+int inf[10001][2];
+
+int max(int a, int b) {
+	return a >= b ? a : b;
+}
+
+int Top_down(int n, int w) {
+	int tmp;
+
+	for (int i = 1; i <= n; ++i) {
+		for (int j = inf[i][0]; j <= w; ++j) {
+			tmp = dp[j - inf[i][0]] + inf[i][1];
+
+			dp[j] = max(dp[j], tmp);
+		}
+	}
+
+	return dp[w];
+}
+
+void solution(void) {
+	int n, w;
+	scanf("%d %d", &n, &w);
+
+	for (int i = 1; i <= n; ++i)
+		scanf("%d %d", &inf[i][0], &inf[i][1]);
+	
+	printf("%d", Top_down(n, w));
+	return;
+}
+
+int main(void) {
+	solution();
+	return 0;
+}
