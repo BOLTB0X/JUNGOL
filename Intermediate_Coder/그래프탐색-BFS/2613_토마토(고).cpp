1f6596a1660015e42904259113927545c4f42705
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-01 16:42:41 +0900

    토마토(고)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1875&sca=3040

--- Intermediate_Coder/그래프탐색-BFS/2613_토마토(고).cpp
+++ Intermediate_Coder/그래프탐색-BFS/2613_토마토(고).cpp
@@ -4,9 +4,14 @@
 
 using namespace std;
 
-queue<pair<int, int>> q;
+typedef struct {
+	int y, x, day;
+} TOMATO;
 
-//»óÇÏÁÂ¿ì
+queue<TOMATO> q;
+int result = 0;
+
+//ìƒí•˜ìš°ì¢Œ
 const int dy[4] = { 1, -1, 0, 0 };
 const int dx[4] = { 0, 0, 1, -1 };
 
@@ -16,16 +21,17 @@ bool in_Range(int y, int x, int m, int n) {
 
 void BFS(vector<vector<int>>& board, int m, int n) {
 	while (!q.empty()) {
-		pair<int, int> cur = q.front();
+		TOMATO cur = q.front();
 		q.pop();
 
 		for (int dir = 0; dir<4;++dir) {
-			int ny = cur.first + dy[dir];
-			int nx = cur.second + dx[dir];
+			int ny = cur.y + dy[dir];
+			int nx = cur.x + dx[dir];
 
 			if (in_Range(ny, nx, m, n) && board[ny][nx] == 0) {
-				board[ny][nx] = board[cur.first][cur.second] + 1;
-				q.push({ ny,nx });
+				board[ny][nx] = 1;
+				result = cur.day + 1;
+				q.push({ ny,nx, cur.day + 1 });
 			}
 		}
 	}
@@ -38,22 +44,20 @@ int solution(vector<vector<int>>& board, int m, int n) {
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
 			if (board[i][j] == 1)
-				q.push({ i,j });
+				q.push({ i,j, 0 });
 		}
 	}
 
 	BFS(board, m, n);
+	answer = result;
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
-			if (board[i][j] == 0)
-				return -1;
-			if (answer < board[i][j])
-				answer = board[i][j];
+			if (board[i][j] == 0) 
+				return-1;
 		}
 	}
-	
-	return answer - 1;
+	return answer;
 }
 
 int main(void) {
