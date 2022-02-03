6f77857e4a28d702041dd9d55e5088537c42365d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-03 23:41:53 +0900

    약수 구하기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=678&sca=2030

--- Beginner_Coder/수학1/1402_약수 구하기.c
+++ Beginner_Coder/수학1/1402_약수 구하기.c
@@ -2,15 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void solution(void) {
-	int n, k;
+int main(void) {
+	int n, k, idx = 0;
 	scanf("%d %d", &n, &k);
 
 	int* arr = malloc(sizeof(int) * (n + 1));
-	for (int i = 0; i <= n; ++i) 
-		arr[i] = 0;
 
-	int idx = 0;
 	for (int i = 1; i <= n; ++i) {
 		if (n % i == 0)
 			arr[idx++] = i;
@@ -22,11 +19,5 @@ void solution(void) {
 		printf("%d\n", arr[k - 1]);
 
 	free(arr);
-
-	return;
-}
-
-int main(void) {
-	solution();
 	return 0;
 }
