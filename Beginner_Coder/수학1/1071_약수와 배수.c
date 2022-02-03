0ef7ea31daa91055797ed09821f3f608ffd44d5b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-03 23:37:53 +0900

    약수와 배수
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=351&sca=2030

--- Beginner_Coder/수학1/1071_약수와 배수.c
+++ Beginner_Coder/수학1/1071_약수와 배수.c
@@ -2,32 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void solution(void) {
+int main(void) {
 	int n, m;
+	int* arr;
+	int res1 = 0, res2 = 0;
+
 	scanf("%d", &n);
 
-	int* arr =(int*)malloc(sizeof(int) * n);
+	arr = (int*)malloc(sizeof(int) * n);
 	for (int i = 0; i < n; ++i)
 		scanf("%d", &arr[i]);
-
 	scanf("%d", &m);
-	int result1 = 0;
-	int result2 = 0;
+
 	for (int i = 0; i < n; ++i) {
 		if (arr[i] <= m && m % arr[i] == 0)
-			result1 += arr[i];
+			res1 += arr[i];
 		if (arr[i] >= m && arr[i] % m == 0)
-			result2 += arr[i];
+			res2 += arr[i];
 	}
-	
-	printf("%d\n", result1);
-	printf("%d\n", result2);
+
+	printf("%d\n", res1);
+	printf("%d\n", res2);
 
 	free(arr);
-	return;
-}
-
-int main(void) {
-	solution();
 	return 0;
 }
