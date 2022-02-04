cf14b70356fa2fb88bc45b5aff40fe503d6b5d2e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-04 23:30:51 +0900

    숫자삼각형
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=914&sca=2020

---
+++ Beginner_Coder/도형만들기2/1641_숫자삼각형.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+void solution(int n, int m) {
+	if (m == 1) {
+		int number = 0, tmp;
+		for (int i = 1; i <= n; ++i) {
+			if (i % 2 == 0) {
+				number += i;
+				tmp = number;
+
+				for (int j = 1; j <= i; ++j)
+					cout << tmp-- << " ";
+			}
+			else {
+				number++;
+				for (int j = 1; j <= i; ++j)
+					cout << number++ << " ";
+				number--;
+			}
+			cout << "\n";
+		}
+	}
+	else if (m == 2) {
+		int number = 0, tmp = 2 * n - 1;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j < 2 * i - 1; ++j)
+				cout << " ";
+			for (int j = 1; j <= tmp; ++j)
+				cout << number << " ";
+			cout << "\n";
+			number++;
+			tmp -= 2;
+		}
+	}
+	
+	else if (m == 3) {
+		for (int i = 1; i <= n / 2; ++i) {
+			int number = 1;
+			for (int j = 1; j <= i; ++j)
+				cout << number++ << " ";
+			cout << "\n";
+		}
+		for (int i = 0; i <= n / 2; ++i) {
+			int number = 1;
+			for (int j = 0; j <= n/2 - i; ++j)
+				cout << number++ << " ";
+			cout << "\n";
+		}
+	}
+
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	if (n >= 1 && n <= 100 && n % 2 != 0 && m>=1 && m <= 3)
+		solution(n, m);
+	else
+		cout << "INPUT ERROR!";
+	return 0;
+}
