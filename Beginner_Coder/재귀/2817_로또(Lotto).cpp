557e477ceb23d7b58cdb7d0ebd1e6de9c1198d3d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-06 13:11:10 +0900

    로또(Lotto)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=2082&sca=2080

---
+++ Beginner_Coder/재귀/2817_로또(Lotto).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> com;
+
+void DFS(int n, vector<int>& numbers, vector<bool>& visited, int cur, int level) {
+	if (level == 6) {
+		for (int& c : com)
+			cout << c << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = cur; i < n; ++i) {
+		if (visited[i])
+			continue;
+		com.push_back(numbers[i]);
+		visited[i] = true;
+		DFS(n, numbers, visited, i + 1, level + 1);
+		visited[i] = false;
+		com.pop_back();
+	}
+
+	return;
+}
+
+void solution(int n, vector<int>& numbers) {
+	vector<bool> visited(n, false);
+
+	DFS(n, numbers, visited, 0, 0);
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	vector<int> numbers;
+
+	cin >> n;
+	numbers.resize(n);
+	for (int& num : numbers)
+		cin >> num;
+
+	solution(n, numbers);
+	return 0;
+}
