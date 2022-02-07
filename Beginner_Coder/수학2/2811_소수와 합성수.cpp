07706542632f7aa1501e670795a384ff46792681
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-07 16:41:30 +0900

    소수와 합성수
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=452&sca=2040

---
+++ Beginner_Coder/수학2/2811_소수와 합성수.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void check(int number) {
+	if (number == 0 || number == 1) {
+		cout << "number one\n";
+		return;
+	}
+
+	for (long long i = 2; i * i <= number; ++i) {
+		if (number % i == 0) {
+			cout << "composite number\n";
+			return;
+		}
+	}
+			
+	cout << "prime number\n";
+	return;
+}
+
+void solution(int* arr) {
+	for (int i = 0; i < 5; ++i)
+		check(arr[i]);
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int arr[5] = { 0, };
+	for (int i = 0; i < 5; ++i)
+		cin >> arr[i];
+
+	solution(arr);
+	return 0;
+}
