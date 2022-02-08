fdf50ae168340e73a7d96613874ea5c65d76f859
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-08 15:13:00 +0900

    로봇
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=285&sca=3040

--- Intermediate_Coder/그래프탐색-BFS/1006_로봇.cpp
+++ Intermediate_Coder/그래프탐색-BFS/1006_로봇.cpp
@@ -4,90 +4,127 @@
 
 using namespace std;
 
+int n, m;
 int board[101][101];
 bool visited[101][101][5];
 
 const int dy[5] = { 0,0,0,1,-1 };
 const int dx[5] = { 0,1,-1,0,0 };
 
-int solution(int m, int n, pair<pair<int, int>, int> start, pair<pair<int, int>, int> end) {
-    int answer = -1;
-    queue<pair<pair<int, int>, pair<int, int>>> q;
-    q.push({ {start.first.first, start.first.second}, {start.second, 0} });
-    visited[start.first.first][start.first.second][start.second] = true;
+int turn_Left(int cd) {
+    if (cd == 1) 
+        return 4;
+    else if (cd == 2) 
+        return 3;
+    else if (cd == 3) 
+        return 1;
+    else if (cd == 4) 
+        return 2;
+}
 
-    while (!q.empty()) {
-        int y = q.front().first.first;
-        int x = q.front().first.second;
-        int dir = q.front().second.first;
-        int cnt = q.front().second.second;
-        q.pop();
+int turn_Right(int cd) {
+    if (cd == 1) 
+        return 3;
+    else if (cd == 2) 
+        return 4;
+    else if (cd == 3) 
+        return 2;
+    else if (cd == 4) 
+        return 1;
+}
 
-        if (y == end.first.first && x == end.first.second && dir == end.second) {
-            answer = cnt;
+int BFS(queue<pair<pair<int, int>, pair<int, int>>>& que, pair<pair<int, int>, int> start, pair<pair<int, int>, int> end) {
+    int result = -1;
+    int sy, sx, sd, ey, ex, ed;
+    sy = start.first.first;
+    sx = start.first.second;
+    sd = start.second;
+    ey = end.first.first;
+    ex = end.first.second;
+    ed = end.second;
+
+    que.push({ {sy,sx},{sd,0} });
+    visited[sy][sx][sd] = true;
+
+    while (!que.empty()) {
+        int cy = que.front().first.first;
+        int cx = que.front().first.second;
+        int cd = que.front().second.first;
+        int cnt = que.front().second.second;
+        que.pop();
+
+        if (cy == ey && cx == ex && cd == ed) {
+            result = cnt;
             break;
         }
 
         for (int k = 1; k <= 3; ++k) {
-            int ny = y + dy[dir] * k;
-            int nx = x + dx[dir] * k;
+            int ny = cy + (dy[cd] * k);
+            int nx = cx + (dx[cd] * k);
 
-            if (visited[ny][nx][dir])
-                continue;
-            if (1 <= ny && ny <= m && 1 <= nx && nx <= n && board[ny][nx] == 0) {
-                visited[ny][nx][dir] = true;
-                q.push({ {ny, nx}, {dir, cnt + 1} });
-            }
-            else
+            //이 방향으로 더이상 진행 불가
+            if (ny < 1 || ny > n || nx < 1 || nx > m)
                 break;
+            //이 방향으로 더이상 진행 불가
+            if (board[ny][nx] == 1)
+                break;
+            //재방문이면 
+            if (visited[ny][nx][cd])
+                continue;
+
+            visited[ny][nx][cd] = 1;
+            que.push({ { ny,nx }, { cd,cnt + 1 } });
         }
 
-        for (int d = 1; d <= 4; ++d) {
-            if (dir != d && !visited[y][x][d]) {
-                visited[y][x][d] = true;
-                if ((dir == 1 && d == 2) || (dir == 2 && d == 1) || (dir == 3 && d == 4) || (dir == 4 && d == 3))
-
-                    q.push({ { y, x }, { d, cnt + 2 } });
-
-                else
-
-                    q.push({ { y, x }, { d, cnt + 1 } });
-            }
+        int nd;
+        nd = turn_Left(cd);
+        if (!visited[cy][cx][nd]) {
+            visited[cy][cx][nd] = true;
+            que.push({ { cy, cx }, { nd,cnt + 1 } });
         }
-
+        
+        nd = turn_Right(cd);
+        if (!visited[cy][cx][nd]) {
+            visited[cy][cx][nd] = true;
+            que.push({ { cy, cx }, { nd,cnt + 1 } });
+        }
     }
+
+    return result;
+}
+
+int solution(pair<pair<int, int>, int> start, pair<pair<int, int>, int> end) {
+    int answer = 0;
+
+    queue<pair<pair<int, int>, pair<int, int>>> que;
+    answer = BFS(que, start, end);
     return answer;
 }
+
 int main(void) {
     ios::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(0);
+    cout.tie(0);
 
-    int m, n;
-    int y, x, d;
-    pair<pair<int, int>, int> start;
-    pair<pair<int, int>, int> end;
-
-    cin >> m >> n;
-
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j)
             cin >> board[i][j];
-        }
     }
 
+    int y, x, d;
+    pair<pair<int, int>, int> start, end;
     cin >> y >> x >> d;
     start.first.first = y;
-    start.first.second = x;
+    start.first.second= x;
     start.second = d;
     
     cin >> y >> x >> d;
     end.first.first = y;
-    end.first.second = x;
+    end.first.second= x;
     end.second = d;
 
-    int ret= solution(m, n, start, end);
-
+    int ret = solution(start, end);
     cout << ret;
     return 0;
 }
