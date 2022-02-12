77500a046594c1c7027e5f1b4fa8bbf7c40f4777
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-12 13:19:19 +0900

    키 순서
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1723&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/2462_키 순서.c
+++ Intermediate_Coder/그래프탐색-DFS/2462_키 순서.c
@@ -3,37 +3,34 @@
 #include <string.h>
 
 int adj[501][501];
-int check[501];
 int visited[501];
+int dist[501];
 
-//깊이우선탐색
-void DFS(int n, int m, int oj, int cur) {
-	if (oj != cur) {
-		check[oj]++;
-		check[cur]++;
+void DFS(int n, int cur, int oj) {
+	if (cur != oj) {
+		dist[cur]++;
+		dist[oj]++;
 	}
 
 	visited[cur] = 1;
 	for (int i = 1; i <= n; ++i) {
-		if (visited[i] == 0 && adj[cur][i] != 0) 
-			DFS(n, m, oj, i);
+		if (visited[i] == 0 && adj[cur][i] == 1) 
+			DFS(n, i, oj);
 	}
 	return;
 }
 
 int solution(int n, int m) {
 	int answer = 0;
+	memset(dist, 0, sizeof(int) * (n + 1));
 
 	for (int i = 1; i <= n; ++i) {
-		//방문리스트 초기화
 		memset(visited, 0, sizeof(int) * (n + 1));
-		//일단 자기 자신들로 호출
-		DFS(n, m, i, i);
+		DFS(n, i, i);
 	}
 
 	for (int i = 1; i <= n; ++i) {
-		//해당 노드의 카운트가 n-1이어야 자신의 등수를 알 수 있음
-		if (check[i] == n - 1)
+		if (dist[i] == n - 1)
 			answer++;
 	}
 
@@ -47,7 +44,7 @@ int main(void) {
 
 	for (int i = 0; i < m; ++i) {
 		scanf("%d %d", &from, &to);
-		adj[from][to] = 1; //작은 놈
+		adj[from][to] = 1;
 	}
 
 	int ret = solution(n, m);
