0f17946f78acbca1058eb4c524639905316ce666
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-12 14:29:53 +0900

    두 줄로 타일 깔기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=687&sca=3060

---
+++ Intermediate_Coder/동적계획법1/1411_두 줄로 타일 깔기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[100001];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 3;
+
+	//╨╦ер╬В
+	for (int i = 3; i <= n; ++i) 
+		dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 20100529;
+	
+	cout << dp[n];
+	return 0;
+}
