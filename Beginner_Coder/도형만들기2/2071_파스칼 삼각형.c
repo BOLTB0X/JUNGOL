f7428ae5e33ca7543a3455cb0bf9ac3255576f30
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-19 23:18:55 +0900

    파스칼 삼각형
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2071

--- Beginner_Coder/도형만들기2/2071_파스칼 삼각형.c
+++ Beginner_Coder/도형만들기2/2071_파스칼 삼각형.c
@@ -1,45 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int pane[30][30];
+int board[31][31];
 
+//파스칼식 채우기
 void fill_triangle(int n) {
 	//초기화
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-			pane[i][j] = 1;
-
-	for (int i = 2; i < n; ++i) {
-		for (int j = 1; j < i; ++j)
-			pane[i][j] = pane[i - 1][j - 1] + pane[i - 1][j];
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) 
+			board[i][j] = 1;
 	}
 
+	for (int i = 2; i < n; ++i) {
+		for (int j = 1; j < i; ++j) {
+			board[i][j] = board[i - 1][j - 1] + board[i - 1][j];
+		}
+	}
 	return;
 }
 
+//출력
 void print_triangle(int n, int m) {
 	if (m == 1) {
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j <= i; ++j) 
-				printf("%d ", pane[i][j]);
+				printf("%d ", board[i][j]);
 			printf("\n");
 		}
 	}
 
 	else if (m == 2) {
 		for (int i = n - 1; i >= 0; --i) {
-			for (int j = 0; j < n - 1 - i; ++j) 
+			for (int j = i; j < n - 1; ++j)
 				printf(" ");
 			for (int j = 0; j <= i; ++j)
-				printf("%d ", pane[i][j]);
+				printf("%d ", board[i][j]);
 			printf("\n");
 		}
 	}
 
 	else {
 		for (int i = n - 1; i >= 0; --i) {
-			for (int j = n-1; j >= i; --j)
-				printf("%d ", pane[j][i]);
+			for (int j = n - 1; j >= i; --j)
+				printf("%d ", board[j][i]);
 			printf("\n");
 		}
 	}
@@ -47,16 +50,16 @@ void print_triangle(int n, int m) {
 	return;
 }
 
-void solution(void) {
-	int n, m;
-	scanf("%d %d", &n, &m);
-
+void solution(int n, int m) {
 	fill_triangle(n);
 	print_triangle(n, m);
 	return;
 }
 
 int main(void) {
-	solution();
+	int n, m;
+	scanf("%d %d", &n, &m);
+
+	solution(n, m);
 	return 0;
 }
