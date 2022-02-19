6a1ffeda4a5e382637cbdf98ff76186925e417ac
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-19 15:56:30 +0900

    진법 변환
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=2375&sca=2040

---
+++ Beginner_Coder/수학2/3106_진법 변환.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#define LL long long
+
+using namespace std;
+
+string get_trans_number(int t1, int t2, string n) {
+	LL tot = 0, size = n.length();
+	string tmp = "", ret = "";
+
+	//t1진수 -> 10진수
+	for (LL i = size - 1, j = 1; i >= 0; --i, j *= t1) {
+		if (n[i] >= 'A' && n[i] <= 'Z')
+			n[i] = '9' + (n[i] - 'A' + 1);
+		tot += j * (n[i] - '0');
+	}
+
+	//10진수 -> t2진수 
+	while (tot > 0) {
+		if (tot % t2 > 9)
+			tmp += (tot % t2) - 10 + 'A';
+		else
+			tmp += (tot % t2) + '0';
+
+		tot /= t2;
+	}
+	size = tmp.length() - 1;
+	//결과 반환을 위한 뒤집기
+	for (LL i = size; i >= 0; --i)
+		ret += tmp[i];
+
+	return ret;
+}
+
+int main(void) {
+	int type1, type2;
+	string number;
+
+	while (1) {
+		cin >> type1;
+		if (type1 == 0)
+			break;
+		cin >> number >> type2;
+
+		string ret = get_trans_number(type1, type2, number);
+
+		cout << ret << '\n';
+	}
+
+	return 0;
+}
