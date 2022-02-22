f7d029e843070e2b48641c8e0132c4754abc148e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-22 13:44:13 +0900

    미로 탐색
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1185&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/1912_미로 탐색.cpp
+++ Intermediate_Coder/그래프탐색-DFS/1912_미로 탐색.cpp
@@ -1,55 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define MLN 100001
+#define ML 3001
 
 using namespace std;
 
-int n, m;
-vector<int> adj[MLN]; //무향 그래프
-vector<int> answer; //정답
-vector<bool> visited(MLN, false); //방문
+vector<int> adj[ML];
 
-void DFS(int cur) {
-	answer.push_back(cur);
-	visited[cur] = true;
+//깊이우선 탐색
+void DFS(int n, vector<bool>& visited,int start) {
+	visited[start] = 1;
+	cout << start << ' ';
 
-	for (int& next : adj[cur]) {
-		if (!visited[next])
-			DFS(next);
+	for (int& next : adj[start]) {
+		if (visited[next])
+			continue;
+		DFS(n, visited, next);
 	}
+
 	return;
 }
 
-void solution(void) {
-	//한 번도 들르지 않은 방이 있다면 그 중 가장 번호가 작은 방
-	for (int i = 1; i <= n; ++i)
-		sort(adj[i].begin(), adj[i].end());
+void solution(int n, int m) {
+	vector<bool> visited(n + 1, false);
 
-	DFS(1);
-
-	for (int& a : answer) 
-		cout << a << ' ';
-	cout << '\n';
-
+	DFS(n, visited, 1);
 	return;
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	int a, b;
-
+	int n, m;
 	cin >> n >> m;
+
 	for (int i = 0; i < m; ++i) {
+		int a, b;
 		cin >> a >> b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);
+		sort(adj[a].begin(), adj[a].end());
+		sort(adj[b].begin(), adj[b].end());
 	}
 
-	solution();
-
+	solution(n, m);
 	return 0;
 }
