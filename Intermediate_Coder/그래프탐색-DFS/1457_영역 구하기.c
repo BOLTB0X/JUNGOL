a35570171da5da138755327fca115acd1aa7eb98
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-23 11:53:33 +0900

    영역 구하기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1457

--- Intermediate_Coder/그래프탐색-DFS/1457_영역 구하기.c
+++ Intermediate_Coder/그래프탐색-DFS/1457_영역 구하기.c
@@ -1,22 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#define ML 101*101
 
 int board[101][101];
 int visited[101][101];
-int cnt = 0;
-int answer[101];
-int ans_idx = 0;
+
+int result[ML], r_idx = 0, cnt = 0;
 
 const int dy[4] = { 1,-1,0,0 };
-const int dx[4] = { 0,0,-1,1 };
+const int dx[4] = { 0,0,1,-1 };
 
 int compare(const void* a, const void* b) {
 	return *(int*)a - *(int*)b;
 }
 
-void DFS(int M, int N, int y, int x) {
+//ì´ˆê¸°í™”
+void init(int m, int n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j)
+			visited[i][j] = 0;
+	}
+
+	return;
+}
+
+void DFS(int m, int n, int y, int x) {
 	visited[y][x] = 1;
 	cnt++;
 
@@ -24,57 +33,48 @@ void DFS(int M, int N, int y, int x) {
 		int ny = y + dy[dir];
 		int nx = x + dx[dir];
 
-		if (ny < 0 || ny >= N || nx < 0 || nx >= M)
-			continue;
-		if (board[ny][nx] != 0 || visited[ny][nx] == 1)
-			continue;
-		DFS(M, N, ny, nx);
+		if (0 <= ny && ny < n && 0 <= nx && nx < m) {
+			if (visited[ny][nx] == 0 && board[ny][nx] == 0)
+				DFS(m, n, ny, nx);
+		}
 	}
 	return;
 }
 
-void solution(void) {
-	//M,NÁ¶½É
-	int M, N, K;
-	scanf("%d %d %d", &M, &N, &K);
+void solution(int m, int n) {
+	init(m, n);
 
-	for (int i = 0; i < N; ++i) {
-		memset(board[i], 0, sizeof(int) * M);
-		memset(visited[i], 0, sizeof(int) * M);
-	}
-
-	for (int i = 0; i < K; ++i) {
-		int y1, x1, y2, x2;
-		scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
-
-		for (int r = y1; r < y2; ++r) {
-			for (int c = x1; c < x2; ++c)
-				board[r][c] = 1;
-		}
-	}
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			if (board[i][j] == 0 && !visited[i][j]) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			if (board[i][j] == 0 && visited[i][j] == 0) {
 				cnt = 0;
-				DFS(M, N, i, j);
-				if(cnt != 0)
-					answer[ans_idx++] = cnt;
+				DFS(m, n, i, j);
+				result[r_idx++] = cnt;
 			}
 		}
 	}
 
-	qsort(answer, ans_idx, sizeof(int), compare);
-
-	printf("%d\n", ans_idx);
-	for (int i = 0; i < ans_idx; ++i) {
-		printf("%d ", answer[i]);
-	}
-
+	qsort(result, r_idx, sizeof(int), compare);
 	return;
 }
 
 int main(void) {
-	solution();
+	int m, n, k;
+	int y1, x1, y2, x2;
+
+	scanf("%d %d %d", &m, &n, &k);
+	for (int i = 0; i < k; ++i) {
+		scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
+		for (int y = y1; y < y2; ++y) {
+			for (int x = x1; x < x2; ++x)
+				board[y][x] = 1;
+		}
+	}
+
+	solution(m, n);
+
+	printf("%d\n", r_idx);
+	for (int i = 0; i < r_idx; ++i)
+		printf("%d ", result[i]);
 	return 0;
 }
