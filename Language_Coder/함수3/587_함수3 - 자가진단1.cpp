c6bd5d7e39bea4d7d83d95dc0b98f77353ba6cbe
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-24 14:17:20 +0900

    기초문제

---
+++ Language_Coder/함수3/587_함수3 - 자가진단1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void recursive_func(int n) {
+	if (n == 0)
+		return;
+	cout << "recursive" << "\n";
+	recursive_func(n - 1);
+	return;
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	recursive_func(n);
+	return 0;
+}
