c1209e4b1edee5d9ecc560a92a395dc7ed564c58
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-24 14:11:20 +0900

    기초문제

---
+++ Language_Coder/함수2/180_함수2 - 형성평가6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void swap(int* a, int* b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return;
+}
+
+void bubble_Sort(int* arr) {
+    //세 번만 반복
+    for (int i = 6; i >= 4; --i) {
+        for (int j = 0; j < 6; ++j) {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+        
+    }
+    return;
+}
+
+int main(void) {
+    int arr[7];
+    for (int i = 0; i < 7; ++i)
+        cin >> arr[i];
+
+    bubble_Sort(arr);
+    for (int i = 0; i < 7; ++i)
+        cout << arr[i] << ' ';
+
+    return 0;
+}
