465cf304533716dd8f375314910fc1bca260b1bc
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-25 16:28:06 +0900

    기초문제

---
+++ Language_Coder/함수3/234_함수3 - 형성평가4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[101];
+
+void recursive_func(int n, int level) {
+	if (n < level)
+		return;
+	
+	arr[level] = (arr[level - 1] * arr[level - 2]) % 100;
+	recursive_func(n, level + 1);
+	return;
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	arr[1] = 1;
+	arr[2] = 2;
+	
+	recursive_func(n, 3);
+	cout << arr[n];
+	
+	return 0;
+}
