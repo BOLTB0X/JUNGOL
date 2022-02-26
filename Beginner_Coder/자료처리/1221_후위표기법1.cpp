dc28b53236e61ce3344b4f18c7d7b64695b9a51f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-26 13:34:41 +0900

    후위표기법1
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1221

--- Beginner_Coder/자료처리/1221_후위표기법1.cpp
+++ Beginner_Coder/자료처리/1221_후위표기법1.cpp
@@ -1,38 +1,48 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
+int stack[11];
+int top = -1;
+
+void push(int data) {
+	stack[++top] = data;
+	return;
+}
+
+int pop(void) {
+	return stack[top--];
+}
+
 int solution(int n, vector<char>& v) {
 	int answer = 0;
-	stack<int> num;
 
-	for (int i = 0; i < v.size(); ++i) {
-		if (v[i] >= '0' && v[i] <= '9')
-			num.push(v[i] - '0');
+	for (char& c : v) {
+		//정수일 경우
+		if (c >= '1' && c <= '9') 
+			push(c - '0');
+		
+		//연산자들 만나다면
 		else {
-			int num1 = num.top();
-			num.pop();
-			int num2 = num.top();
-			num.pop();
-
-			if (v[i] == '+')
-				num.push(num2 + num1);
-			else if (v[i] == '-')
-				num.push(num2 - num1);
-			else if (v[i] == '*')
-				num.push(num2 * num1);
-			else if (v[i] == '/') 
-				num.push(num2 / num1);
+			if (c == '+') 
+				stack[top] += pop();
+			else if (c == '-')
+				stack[top] -= pop();
+			else if (c == '*')
+				stack[top] *= pop();
+			else if (c == '/')
+				stack[top] /= pop();
 		}
 	}
-
-	answer = num.top();
+	
+	//최상단 pop
+	answer = pop();
 	return answer;
 }
 
 int main(void) {
+	//초기화
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
