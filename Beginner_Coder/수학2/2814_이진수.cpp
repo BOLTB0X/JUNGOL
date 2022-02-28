2d65b3af4ea7197211fb2ae9d300787039c5cef1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-28 20:09:14 +0900

    이진수
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2814&sca=2040

---
+++ Beginner_Coder/수학2/2814_이진수.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//재귀버전
+int recursive_func(string bin, int idx) {
+	if (idx == 0)
+		return 0;
+	return recursive_func(bin, idx - 1) * 2 + (bin[idx - 1] - '0');
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string input;
+	cin >> input;
+
+	int tot = 0;
+	int size = input.size();
+
+	//반복문 버전
+	for (int i = size - 1, j = 1; i >= 0; i--, j *= 2) 
+		tot += ((input[i] - '0') * j);
+	//cout << tot;
+	cout << recursive_func(input, size); //재귀
+	return 0;
+}
