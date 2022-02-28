8fba58e5d1cafca1e75f8a9e75d32baaf52d8d76
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-28 14:11:52 +0900

    등수 찾기(ranking)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=3428&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/3428_등수 찾기(ranking).cpp
+++ Intermediate_Coder/그래프탐색-DFS/3428_등수 찾기(ranking).cpp
@@ -5,59 +5,60 @@
 using namespace std;
 
 int n, m, x;
-vector<int> adj[100001];
-vector<int> r_adj[100001];
+vector<int> adj[100001]; //나보다 큰 넘들
+vector<int> r_adj[100001]; //나보다 작은 넘들
 bool visited[100001];
 
 int cnt = 0;
 int r_cnt = 0;
 
 void DFS(int cur) {
-	if (visited[cur])
-		return;
-	visited[cur] = true;
-	cnt++;
+    if (visited[cur])
+        return;
+    visited[cur] = true;
+    cnt++;
 
-	for (int& next : adj[cur])
-		DFS(next);
-	return;
+    for (int& next : adj[cur])
+        DFS(next);
+    return;
 }
 
 void R_DFS(int cur) {
-	if (visited[cur])
-		return;
-	visited[cur] = true;
-	r_cnt++;
+    if (visited[cur])
+        return;
+    visited[cur] = true;
+    r_cnt++;
 
-	for (int& next : r_adj[cur])
-		R_DFS(next);
-	return;
+    for (int& next : r_adj[cur])
+        R_DFS(next);
+    return;
 }
 
 void solution(void) {
-	memset(visited, 0, sizeof(bool) * n + 1);
-	DFS(x);
-	memset(visited, 0, sizeof(bool) * n + 1);
-	R_DFS(x);
+    memset(visited, 0, sizeof(bool) * n + 1);
+    DFS(x);
+    memset(visited, 0, sizeof(bool) * n + 1);
+    R_DFS(x);
 
-	cout << r_cnt << ' ' << n - cnt + 1 << '\n';
-	return;
+    cout << r_cnt << ' ' << n - cnt + 1 << '\n';
+    return;
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    //초기화
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m >> x;
+    cin >> n >> m >> x;
 
-	for (int i = 0; i < m; ++i) {
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-		r_adj[b].push_back(a);
-	}
+    for (int i = 0; i < m; ++i) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        r_adj[b].push_back(a);
+    }
 
-	solution();
-	return 0;
+    solution();
+    return 0;
 }
