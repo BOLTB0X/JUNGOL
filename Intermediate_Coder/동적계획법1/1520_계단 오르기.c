eb0107bfbb07d6ffcca3b3e722c7ecd10b0d7446
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-01 15:10:57 +0900

    계단 오르기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1520&sca=sca

--- Intermediate_Coder/동적계획법1/1520_계단 오르기.c
+++ Intermediate_Coder/동적계획법1/1520_계단 오르기.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//최댓값 반환
 int MAX(int a, int b) {
 	return a > b ? a : b;
 }
 
-void solution(void) {
-	int n;
-	scanf("%d", &n);
+int solution(int n) {
+	int answer = 0;
 	int* dp = malloc(sizeof(int) * (n + 1));
 	int* steps = malloc(sizeof(int) * (n + 1));
 	
@@ -16,23 +16,28 @@ void solution(void) {
 		scanf("%d", &steps[i]);
 
 	dp[1] = steps[1];
-	dp[2] = steps[1]+steps[2];
+	dp[2] = steps[1] + steps[2];
 	dp[3] = MAX(steps[3] + steps[1], steps[2] + steps[3]);
 
+	//보텀업 방식
+	//현재 계단을 뭐로 더할지가 관건
+	for (int i = 4; i <= n; ++i)
+		dp[i] = steps[i] + MAX(dp[i - 3] + steps[i - 1], dp[i - 2]);
 
-	//보텀업
-	//바로 전 칸을 밟을 지 말지 결정
-	for (int i = 4; i <= n; ++i) {
-		dp[i] = steps[i] + MAX(dp[i -3] + steps[i - 1], dp[i - 2]);
-	}
-
-	printf("%d", dp[n]);
+	answer = dp[n];
 
 	free(dp);
 	free(steps);
+
+	return answer;
 }
 
 int main(void) {
-	solution();
+	int n;
+	scanf("%d", &n);
+
+	int ret = solution(n);
+
+	printf("%d", ret);
 	return 0;
 }
