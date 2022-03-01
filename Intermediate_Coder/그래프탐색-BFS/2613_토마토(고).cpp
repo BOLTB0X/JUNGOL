7aa0b4ba91660cf78ca4601f7c7c202bc87c6784
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-01 14:52:46 +0900

    토마토(고)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2613&sca=3040

--- Intermediate_Coder/그래프탐색-BFS/2613_토마토(고).cpp
+++ Intermediate_Coder/그래프탐색-BFS/2613_토마토(고).cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#define ML 1000001
 
 using namespace std;
 
@@ -8,21 +8,33 @@ typedef struct {
 	int y, x, day;
 } TOMATO;
 
-queue<TOMATO> q;
+TOMATO que[ML];
+int fr = 0, re = 0;
+
+void enqueue(TOMATO data) {
+	que[re++] = data;
+	return;
+}
+
+TOMATO dequeue(void) {
+	return que[fr++];
+}
+
 int result = 0;
 
-//상하우좌
+//상하좌우
 const int dy[4] = { 1, -1, 0, 0 };
 const int dx[4] = { 0, 0, 1, -1 };
 
-bool in_Range(int y, int x, int m, int n) {
+//범위 반환
+int in_Range(int y, int x, int m, int n) {
 	return  0 <= y && y < n && 0 <= x && x < m;
 }
 
+//너비우선 탐색
 void BFS(vector<vector<int>>& board, int m, int n) {
-	while (!q.empty()) {
-		TOMATO cur = q.front();
-		q.pop();
+	while (fr < re) {
+		TOMATO cur = dequeue();
 
 		for (int dir = 0; dir<4;++dir) {
 			int ny = cur.y + dy[dir];
@@ -31,7 +43,7 @@ void BFS(vector<vector<int>>& board, int m, int n) {
 			if (in_Range(ny, nx, m, n) && board[ny][nx] == 0) {
 				board[ny][nx] = 1;
 				result = cur.day + 1;
-				q.push({ ny,nx, cur.day + 1 });
+				enqueue({ ny,nx, cur.day + 1 });
 			}
 		}
 	}
@@ -43,24 +55,29 @@ int solution(vector<vector<int>>& board, int m, int n) {
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
+			//토마토 좌표 
 			if (board[i][j] == 1)
-				q.push({ i,j, 0 });
+				enqueue({ i,j, 0 });
 		}
 	}
 
 	BFS(board, m, n);
 	answer = result;
 
+	//안익은 토마토가 있으면
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
 			if (board[i][j] == 0) 
-				return-1;
+				return -1;
 		}
 	}
+
+	//반환
 	return answer;
 }
 
 int main(void) {
+	//초기화
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
