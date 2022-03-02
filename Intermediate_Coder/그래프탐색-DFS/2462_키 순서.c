9a123a28b14a36af8cf096a067bbef36d12e0880
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-02 13:13:18 +0900

    키 순서
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2462&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/2462_키 순서.c
+++ Intermediate_Coder/그래프탐색-DFS/2462_키 순서.c
@@ -4,59 +4,66 @@
 
 int adj[ML][ML];
 int visited[ML];
-int dist[ML];
+int cnt[ML];
 
-//DFS로 탐색
-void DFS(int n, int cur, int oj) {
-	if (cur != oj) {
-		dist[cur]++;
-		dist[oj]++;
+void init(int n) {
+	for (int i = 1; i <= n; ++i) 
+		visited[i] = 0;
+	return;
+}
+
+//깊이 우선 탐색으로 object 기준으로 키 비교
+void DFS(int n, int m, int student, int object) {
+	//카운트 기준
+	if (student != object) {
+		cnt[student]++;
+		cnt[object]++;
 	}
 
-	visited[cur] = 1;
+	visited[student] = 1;
 	for (int i = 1; i <= n; ++i) {
-		if (adj[cur][i] == 0)
+		//비교할 학생이 없다면
+		if (adj[student][i] == 0)
 			continue;
+		//재방문
 		if (visited[i] == 1)
 			continue;
-		DFS(n, i, oj);
+		DFS(n, m, i, object);
 	}
 	return;
 }
 
 int solution(int n, int m) {
 	int answer = 0;
-
-	//거리, 방문리스트 초기화
-	for (int i = 0; i <= n; ++i) {
-		dist[i] = 0;
-		visited[i] = 0;
-	}
-
-	//각 노드를 시작으로 거리리스트에 방문횟수 저장
-	for (int i = 1; i <= n; ++i) {
-		DFS(n, i, i);
-		//방문리스트 초기화
-		for (int i = 0; i <= n; ++i)
-			visited[i] = 0;
-	}
 	
+	//방문리스트 및 카운트 초기화
+	for (int i = 1; i <= n; ++i) {
+		visited[i] = 0;
+		cnt[i] = 0;
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		DFS(n, m, i, i);
+		init(n); //방문리스트 초기화
+	}
+
 	//자기 자신을 제외하고 등수 비교가 가능하는 경우는 dist -> n-1인경우
 	for (int i = 1; i <= n; ++i) {
-		if (dist[i] == n - 1)
+		if (cnt[i] == n - 1)
 			answer++;
 	}
+
 	return answer;
 }
 
 int main(void) {
-	int n, m, from, to;
+	int n, m, a, b;
 
 	scanf("%d %d", &n, &m);
 
 	for (int i = 0; i < m; ++i) {
-		scanf("%d %d", &from, &to);
-		adj[from][to] = 1;
+		scanf("%d %d", &a, &b);
+		adj[a][b] = 1; // a학생이 b학생보다 키가 작다
 	}
 
 	int ret = solution(n, m);
