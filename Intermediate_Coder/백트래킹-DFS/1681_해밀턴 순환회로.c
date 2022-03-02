fecbdd57b0196b174b8475056d52e318cb790bb0
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-02 14:46:02 +0900

    해밀턴 순환회로
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1681&sca=3030

--- Intermediate_Coder/백트래킹-DFS/1681_해밀턴 순환회로.c
+++ Intermediate_Coder/백트래킹-DFS/1681_해밀턴 순환회로.c
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
-int n, flag = 0, result = 0x7fffffff;
+int n, flag = 0, result = 2e9;
 int board[14][14];
 int visited[14];
 
-//DFS로 백트래킹
+//백트래킹을 위한 DFS
 void DFS(int cur, int level, int tot) {
 	if (level == n) {
 		//마지막 돌아가는 곳이 0이 아니고 기존 비용보다 더 작다면
@@ -22,8 +21,7 @@ void DFS(int cur, int level, int tot) {
 		if (visited[i] == 1)
 			continue;
 
-		//이동불가
-		//장소 사이에 이동할 수 있는 방법이 없다면 비용을 0으로 표시
+		//자기 자신이므로
 		if (board[cur][i] == 0)
 			continue;
 
@@ -33,21 +31,26 @@ void DFS(int cur, int level, int tot) {
 
 		visited[i] = 1;
 		DFS(i, level + 1, tot + board[cur][i]);
-		visited[i] = 0;
+		visited[i] = 0; //백트래킹
 	}
-	
 	return;
 }
 
 int solution(void) {
 	int answer = 0;
-	//방문리스트 초기화
-	memset(visited, 0, sizeof(int) * 14);
+
+	//초기화
+	for (int i = 1; i <= n; ++i) 
+		visited[i] = 0;
+	
 	//백트래킹 시작
 	DFS(1, 1, 0);
 
-	answer = result;
-	return answer;
+	//도달 했다면
+	if (flag == 1)
+		answer = result;
+
+	return answer; //반환
 }
 
 int main(void) {
@@ -59,10 +62,6 @@ int main(void) {
 	}
 
 	int ret = solution();
-
-	//도달하지 못한 경우
-	if (flag == 0)
-		ret = 0;
 
 	printf("%d", ret);
 	return 0;
