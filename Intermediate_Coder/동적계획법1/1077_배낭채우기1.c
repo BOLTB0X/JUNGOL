4c07a383f8ec5653f5e047daa898d99132e65df4
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-04 15:24:47 +0900

    배낭채우기1
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=357&sca=3060

--- Intermediate_Coder/동적계획법1/1077_배낭채우기1.c
+++ Intermediate_Coder/동적계획법1/1077_배낭채우기1.c
@@ -9,22 +9,24 @@ int MAX(int a, int b) {
 	return a >= b ? a : b;
 }
 
-void solution(int n, int w) {
+int solution(int n, int w) {
+	//초기화
 	for (int i = 1; i <= w; ++i)
 		dp[i] = 0;
+
 	//보텀업
 	//n번 반복해야함
 	for (int i = 1; i <= n; ++i) {
+		//보석의 무게를 제한선 w까지 반복
 		//현재 보석의 무게 부터 데드드라인까지 보텀업 방식
 		for (int j = inf[i][0]; j <= w; ++j) {
 			//dp테이블에 해당 무게값을 누적으로 더해줌 -> 중복 허용이므로
-			int tmp = dp[j - inf[i][0]] + inf[i][1]; 
-			dp[j] = MAX(dp[j], tmp);
+			int tmp = dp[j - inf[i][0]] + inf[i][1];
+			dp[j] = MAX(tmp, dp[j]);
 		}
 	}
 	
-	printf("%d", dp[w]);
-	return;
+	return dp[w];
 }
 
 int main(void) {
@@ -33,6 +35,8 @@ int main(void) {
 
 	for (int i = 1; i <= n; ++i)
 		scanf("%d %d", &inf[i][0], &inf[i][1]);
-	solution(n, w);
+
+	int ret = solution(n, w);
+	printf("%d", ret);
 	return 0;
 }
