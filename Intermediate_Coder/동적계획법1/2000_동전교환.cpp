b203be4f110727ffe546fb324cb4a838143a94cd
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-04 15:16:06 +0900

    동전교환
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2000&sca=3060

--- Intermediate_Coder/동적계획법1/2000_동전교환.cpp
+++ Intermediate_Coder/동적계획법1/2000_동전교환.cpp
@@ -7,21 +7,25 @@ vector<int> dp;
 vector<int> coins;
 
 void solution(int n, int target) {
+	dp.resize(target + 1, target + 1); //최솟값을 위해 최댓값 셋팅
+
+	//보텀업
 	//잔돈 1부터 target까지
-	//보텀업 방식
 	for (int i = 1; i <= target; ++i) {
 		//현재 타겟을 코인들로 거스러 줄 수 있는지
 		for (int j = 1; j <= n; ++j) {
-			//i과 동전이 같다면
+			//타겟과 동전의 가치가 같다면
 			if (i == coins[j]) {
-				dp[i] = 1;
+				dp[i] = 1; //1
 				break;
 			}
-			//타겟 - 동전이 0보다 크고 기존 dp값보다 작다면
-			if (i - coins[j] > 0 && dp[i] > dp[i - coins[j]])
+
+			//최소 갯수를 구해야하므로
+			if (i - coins[j] > 0 && dp[i] > dp[i - coins[j]] + 1)
 				dp[i] = dp[i - coins[j]] + 1;
 		}
 	}
+
 	return;
 }
 
