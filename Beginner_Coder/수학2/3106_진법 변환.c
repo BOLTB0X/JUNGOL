55a4bbc43f5968a04edee62648640894b6a17064
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-05 17:14:06 +0900

    진법 변환
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=3106&sca=2040

--- Beginner_Coder/수학2/3106_진법 변환.c
+++ Beginner_Coder/수학2/3106_진법 변환.c
@@ -2,59 +2,63 @@
 #include <stdio.h>
 #include <string.h>
 
-long long trans_num(int type, char* n) {
+//10진법 변환
+long long trans_dec(int type1, char* number) {
 	long long tot = 0;
-
 	long long i, j;
-	for (i = strlen(n) - 1, j = 1; i >= 0; --i, j *= type) {
-		if (n[i] >= 'A') 
-			n[i] = '9' + (n[i] - 'A') + 1;
-		tot += j * (n[i] - '0');
+	
+	for (i = strlen(number) - 1, j = 1; i >= 0; --i, j *= type1) {
+		if (number[i] >= 'A') 
+			number[i] = '9' + (number[i] - 'A') + 1;
+		tot += j * (number[i] - '0');
 	}
 
 	return tot;
 }
 
-void trans_type2(int type, long long n) {
-	char num[1001];
-
+//10진법 - > type2
+void trans_type2(int type2, long long dec) {
+	char res[1001];
 	long long idx = 0;
-	while (n > 0) {
-		if (n % type > 9)
-			num[idx++] = (n % type) - 10 + 'A';
-		else
-			num[idx++] = (n % type) + '0';
-		n /= type;
-	}
-	num[idx] = '\0';
 
-	for (long long i = strlen(num) - 1; i >= 0; --i) {
-		printf("%c", num[i]);
+	while (dec > 0) {
+		int mod = dec % type2;
+
+		if (mod > 9) //10진법을 넘어가면
+			res[idx++] = mod - 10 + 'A';
+		else
+			res[idx++] = mod + '0';
+
+		dec /= type2;
 	}
+
+	res[idx] = '\0';
+
+	for (long long i = strlen(res) - 1; i >= 0; --i)
+		printf("%c", res[i]);
 	printf("\n");
+
 	return;
 }
 
 void solution(void) {
 	int type1, type2;
-	char num[1000001];
+	char number[1000001];
 
 	while (1) {
 		scanf("%d", &type1);
 
 		if (type1 == 0)
 			break;
+		scanf("%s %d", number, &type2);
 
-		scanf("%s %d", num, &type2);
-
-		if (num[0] == '0') {
+		if (number[0] == '0') {
 			printf("0\n");
 			continue;
 		}
 
-		trans_type2(type2, trans_num(type1, num));
+		trans_type2(type2, trans_dec(type1, number));
 	}
-	return;
 }
 
 int main(void) {
