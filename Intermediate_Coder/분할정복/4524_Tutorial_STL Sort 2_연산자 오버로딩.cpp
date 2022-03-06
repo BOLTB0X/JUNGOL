a82280b78ba542ec022813ad69a75a891f9aaacf
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-06 16:48:32 +0900

    Tutorial_STL Sort 2
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=4524&sca=3010

---
+++ Intermediate_Coder/분할정복/4524_Tutorial_STL Sort 2_연산자 오버로딩.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[100000];
+
+int compare(int &a, int &b) {
+    return a > b;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &arr[i]);
+
+    //연산자 오버로딩 이용
+    sort(arr, arr + n, compare);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
