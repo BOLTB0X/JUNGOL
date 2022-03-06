9fe9a90de67ec1d4add4ca75cb11d0872982e795
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-06 16:31:15 +0900

    Tutorial : 퀵소트(Quick Sort 빠른정렬)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=2858&sca=3010

--- Intermediate_Coder/분할정복/3518_Tutorial_퀵소트(Quick Sort 빠른정렬).c
+++ Intermediate_Coder/분할정복/3518_Tutorial_퀵소트(Quick Sort 빠른정렬).c
@@ -4,6 +4,14 @@
 int n;
 int arr[10000];
 
+//출력
+void print_arr(void) {
+	for (int i = 0; i < n; ++i)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+//스와핑
 void swap(int* a, int* b) {
 	int tmp = *a;
 	*a = *b;
@@ -11,13 +19,9 @@ void swap(int* a, int* b) {
 	return;
 }
 
-void print_arr(void) {
-	for (int i = 0; i < n; ++i)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
-
+//퀵정렬
 void quick_Sort(int start, int end) {
+	//탈출 조건
 	if (start >= end)
 		return;
 
@@ -28,11 +32,12 @@ void quick_Sort(int start, int end) {
 		if (arr[j] < pivot)
 			swap(&arr[i++], &arr[j]);
 	}
-	swap(&arr[i], &arr[end]);
+	swap(&arr[i], &arr[end]); //스와핑
 
 	print_arr();
 
-	quick_Sort(start, i - 1);
+	//반으로 잘라서 다시
+	quick_Sort(start, i - 1); 
 	quick_Sort(i + 1, end);
 	return;
 }
