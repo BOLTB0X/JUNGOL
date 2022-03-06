95dde81356b3b1446ee0aa1f673d6cc82fbb4199
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-06 16:34:46 +0900

    Tutorial: STL Sort 1
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=4523&sca=3010

--- Intermediate_Coder/분할정복/4523_Tutorial_STL Sort 1.cpp
+++ Intermediate_Coder/분할정복/4523_Tutorial_STL Sort 1.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int arr[100000];
 
+void print_arr(int n) {
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << ' ';
+	return;
+}
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -17,16 +23,14 @@ int main(void) {
 		cin >> arr[i];
 	cin >> start >> end;
 
-	
+	//일정 구간만 정렬
 	sort(arr + start, arr + end + 1);
 
-	for (int i = 0; i < n; ++i)
-		cout << arr[i] << ' ';
+	print_arr(n);
 	cout << "\n";
 	
 	sort(arr, arr + n);
+	print_arr(n);
 
-	for (int i = 0; i < n; ++i)
-		cout << arr[i] << ' ';
 	return 0;
 }
