5e21fab1447433a828c4018fa79640183f131632
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-06 14:45:29 +0900

    미로 탐색
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1912&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/1912_미로 탐색.cpp
+++ Intermediate_Coder/그래프탐색-DFS/1912_미로 탐색.cpp
@@ -1,46 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define ML 3001
+#define MS 100001 #최대 길이
 
 using namespace std;
 
-vector<int> adj[ML];
+vector<int> adj[MS];
 
-//깊이우선 탐색
-void DFS(int n, vector<bool>& visited,int start) {
-	visited[start] = 1;
-	cout << start << ' ';
+//깊이우선탐색 
+void DFS(int n, vector<int>& visited, int cur, vector<int>& path) {
+	path.push_back(cur);
+	visited[cur] = 1; //재방문
 
-	for (int& next : adj[start]) {
-		if (visited[next])
+	for (int& next : adj[cur]) {
+		//재방문 
+		if (visited[next] == 1)
 			continue;
-		DFS(n, visited, next);
-	}
 
+		//다시 호출
+		DFS(n, visited, next, path);
+	}
 	return;
+
 }
 
 void solution(int n, int m) {
-	vector<bool> visited(n + 1, false);
+	vector<int> answer;
+	vector<int> visited(n + 1, 0);
 
-	DFS(n, visited, 1);
+	//정렬 - > 작은 수부터 진행하는 게
+	for (int i = 1; i <= n; ++i)
+		sort(adj[i].begin(), adj[i].end());
+
+	//깊이우선 탐색으로 탐색 시작
+	DFS(n, visited, 1, answer);
+
+	for (int& a : answer)
+		cout << a << ' ';
+
 	return;
 }
 
 int main(void) {
 	int n, m;
+	int a, b;
 	cin >> n >> m;
 
 	for (int i = 0; i < m; ++i) {
-		int a, b;
 		cin >> a >> b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);
-		sort(adj[a].begin(), adj[a].end());
-		sort(adj[b].begin(), adj[b].end());
 	}
 
 	solution(n, m);
+
 	return 0;
 }
