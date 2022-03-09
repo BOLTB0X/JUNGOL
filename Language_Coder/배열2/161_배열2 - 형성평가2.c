f346cb916b954021c70f106eb225e73f2c7137b3
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-09 22:10:28 +0900

    기초문제

--- Language_Coder/배열2/161_배열2 - 형성평가2.c
+++ Language_Coder/배열2/161_배열2 - 형성평가2.c
@@ -9,12 +9,14 @@ int main(void) {
 		scanf("%d", &val);
 		if (val == 0)
 			break;
+		
 		arr[val/10]++;
 	}
 
 	for (int i = 10; i >= 0; --i) {
 		if (arr[i] == 0)
 			continue;
+		
 		printf("%d : %d person\n", i * 10, arr[i]);
 	}
 
