7675a9deabee55a1216e208bce5befda34278eef
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-10 14:05:21 +0900

    두 줄로 타일 깔기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1411&sca=3060

--- Intermediate_Coder/동적계획법1/1411_두 줄로 타일 깔기.c
+++ Intermediate_Coder/동적계획법1/1411_두 줄로 타일 깔기.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 
 int dp[100001] = { 0, };
+const int mod = 20100529;
 
-void solution(void) {
-	int n;
-	scanf("%d", &n);
-
+void solution(int n) {
 	dp[1] = 1;
 	dp[2] = 3;
 
 	for (int i = 3; i <= n; ++i) 
-		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 20100529;
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % mod;
 	
-	printf("%d\n", dp[n]);
 	return;
 }
 
 int main(void) {
-	solution();
+	int n;
+	scanf("%d", &n);
+
+	solution(n);
+
+	printf("%d\n", dp[n]);
 	return 0;
 }
