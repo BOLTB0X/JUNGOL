9b0cf3e1f8930d9f66f73c043c8741c10b3f62df
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-11 15:39:10 +0900

    회장뽑기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=816&sca=4070

--- Advanced_Coder/그래프알고리즘/1544_회장뽑기_Floyd.cpp
+++ Advanced_Coder/그래프알고리즘/1544_회장뽑기_Floyd.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define INF 10000000
+#define INF 1e9
 
 using namespace std;
 
 int result;
 int adj[51][51];
 
-//«√∑Œ¿ÃµÂ øÕº£ æÀ∞Ì∏Æ¡Ú
 void floyd(int n, vector<int>& v) {
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			for (int k = 1; k <= n; k++)
-				adj[j][k] = min(adj[j][k], adj[j][i] + adj[i][k]);
+	for (int k = 1; k <= n; ++k) {
+		for (int a = 1; a <= n; ++a) {
+			for (int b = 1; b <= n; ++b)
+				adj[a][b] = min(adj[a][b], adj[a][k] + adj[k][b]);
 		}
 	}
 
 	result = INF;
+	
 	for (int i = 1; i <= n; ++i) {
 		int tmp = 0;
 		for (int j = 1; j <= n; ++j)
 			tmp = max(tmp, adj[i][j]);
 
-		//√÷º“∫ÒøÎ
 		if (tmp < result) {
 			v.clear();
 			result = tmp;
@@ -40,19 +39,18 @@ void floyd(int n, vector<int>& v) {
 vector<int> solution(int n) {
 	vector<int> answer;
 
-	floyd(n, answer); //«√∑Œ¿ÃµÂ
-	//¡§∑ƒ
-	sort(answer.begin(), answer.end());
+	floyd(n, answer);
 
 	return answer;
 }
 
 int main(void) {
+	//Ï¥àÍ∏∞Ìôî
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	int n, a, b;
+	int n;
 	cin >> n;
 
 	for (int i = 1; i <= n; ++i) {
@@ -63,16 +61,21 @@ int main(void) {
 	}
 
 	while (1) {
+		int a, b;
 		cin >> a >> b;
-		if (a == - 1 || b == -1)
+
+		if (a == -1 || b == -1)
 			break;
+
 		adj[a][b] = 1;
 		adj[b][a] = 1;
 	}
 
 	vector<int> ret = solution(n);
 	cout << result << " " << ret.size() << '\n';
+	sort(ret.begin(), ret.end());
 	for (int& r : ret)
 		cout << r << ' ';
 	return 0;
+
 }
