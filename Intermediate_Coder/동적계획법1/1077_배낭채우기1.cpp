842ef659c5bb50960e8febe7700118ddba61c2ce
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-14 23:08:56 +0900

    배낭채우기1
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1077&sca=3060

--- Intermediate_Coder/동적계획법1/1077_배낭채우기1.cpp
+++ Intermediate_Coder/동적계획법1/1077_배낭채우기1.cpp
@@ -3,43 +3,56 @@
 
 using namespace std;
 
-int solution(int n, int w, vector<pair<int, int>>& inf) {
-	int answer = 0;
-	//dp테이블 초기화
-	vector<int> dp(w + 1, 0);
+//최댓값 반환
+int Max(int a, int b) {
+	return a > b ? a : b;
+}
 
-	//보텀업 방식
+int solution(int n, int w, vector<pair<int, int>>& arr) {
+	int answer = 0;
+	//dp 테이블 초기화
+	vector<int> dp(w + 1, 0);
+	vector<int> wi(n + 1, 0); //각 보석의 무게
+	vector<int> pi(n + 1, 0); //각 보석의 값어치
+
+	for (int i = 0; i < n;++i) {
+		wi[i + 1] = arr[i].first;
+		pi[i + 1] = arr[i].second;
+	}
+
+	//보텀업
 	for (int i = 1; i <= n; ++i) {
-		//해당 i번 가방내 중복허용하고 dp테이블을 채움
-		for (int j = inf[i].first; j <= w; ++j) {
-			//제한선 내 인경우
-			if (j >= inf[i].first) {
-				int tmp = dp[j - inf[i].first] + inf[i].second;
-				dp[j] = tmp > dp[j] ? tmp : dp[j];
-			}
+		//현재 보석으러만 채워보면서
+		//최댓값으로 교체
+		for (int j = 1; j <= w; ++j) {
+			//제한선 내이면
+			if (j - wi[i] >= 0) 
+				dp[j] = Max(dp[j], dp[j - wi[i]] + pi[i]);
 		}
 	}
 
+	answer = dp[w];
 	return answer;
 }
 
 int main(void) {
-	//초기화
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	int n, w; //보석 가지 수, 최대 용량
-	vector<pair<int, int>> inf; //무게와 값어치
-
+	int n, w;
+	vector<pair<int, int>> arr;
 	cin >> n >> w;
-	inf.resize(n + 1);
 
-	for (int i = 1; i <= n; ++i) 
-		cin >> inf[i].first >> inf[i].second;
+	for (int i = 0; i < n; ++i) {
+		int a, b;
 
-	int ret = solution(n, w, inf);
+		cin >> a >> b;
+		arr.push_back({ a,b });
+	}
+
+	int ret = solution(n, w, arr);
 	cout << ret;
-	
+
 	return 0;
 }
