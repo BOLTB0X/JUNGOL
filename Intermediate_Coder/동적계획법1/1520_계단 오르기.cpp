d11df4781913069930915b395772063d9e88d391
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-14 21:57:44 +0900

    계단 오르기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1520&sca=3060

--- Intermediate_Coder/동적계획법1/1520_계단 오르기.cpp
+++ Intermediate_Coder/동적계획법1/1520_계단 오르기.cpp
@@ -1,47 +1,30 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<int> dp;
-vector<int> steps;
+int steps[301];
+int dp[301] = { 0, };
 
 //최댓값 반환
-int MAX(int a, int b) {
+int Max(int a, int b) {
 	return a > b ? a : b;
 }
 
-void solution(int n) {
-	dp.resize(n + 1, 0);
+int main(void) {
+	int n;
+
+	cin >> n;
+	for (int i = 1; i <= n; ++i) 
+		cin >> steps[i];
 
 	dp[1] = steps[1];
 	dp[2] = steps[1] + steps[2];
-	dp[3] = MAX(steps[1] + steps[3], steps[2] + steps[3]);
+	dp[3] = Max(steps[1] + steps[3], steps[2] + steps[3]);
 
-	//보텀업
-	//현재 계단을 밟을 지 말지 결정
-	//현재 계단을 더할 것 중에 연속 3칸은 안 되므로
-	// 세 칸 전 기록 + 현재 -1 , 두칸 전 기록 
+	//보텀업 
 	for (int i = 4; i <= n; ++i) 
-		dp[i] = steps[i] + MAX(dp[i - 3] + steps[i - 1], dp[i - 2]);
+		dp[i] = steps[i] + Max(dp[i - 3] + steps[i - 1], dp[i - 2]);
 	
 	cout << dp[n];
-	return;
-}
-
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	int n;
-	cin >> n;
-	steps.resize(n + 1, 0);
-
-	for (int i = 1; i <= n; ++i)
-		cin >> steps[i];
-
-	solution(n);
-
 	return 0;
 }
