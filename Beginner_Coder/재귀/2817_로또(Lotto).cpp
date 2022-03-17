e9ee1b7638abfbca4d1b25ace99dc78a80a294ed
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-17 22:55:40 +0900

    로또(Lotto)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2817&sca=2080

--- Beginner_Coder/재귀/2817_로또(Lotto).cpp
+++ Beginner_Coder/재귀/2817_로또(Lotto).cpp
@@ -3,24 +3,25 @@
 
 using namespace std;
 
-vector<int> com;
-
 //백트래킹 이용
 void DFS(int n, vector<int>& numbers, vector<bool>& visited, int cur, int level) {
 	if (level == 6) {
-		for (int& c : com)
-			cout << c << ' ';
+		for (int i = 0; i < n; ++i) {
+			//방문여부로 출력 가능
+			if (visited[i]) 
+				cout << numbers[i] << ' ';
+		}
 		cout << '\n';
 		return;
 	}
 
 	for (int i = cur; i < n; ++i) {
+		//재방문
 		if (visited[i])
 			continue;
+
 		visited[i] = true;
-		com.push_back(numbers[i]);
 		DFS(n, numbers, visited, i + 1, level + 1);
-		com.pop_back();
 		visited[i] = false;
 	}
 
@@ -28,7 +29,7 @@ void DFS(int n, vector<int>& numbers, vector<bool>& visited, int cur, int level)
 }
 
 void solution(int n, vector<int>& numbers) {
-	vector<bool> visited(n, false);
+	vector<bool> visited(n, false); //방문리스트
 
 	DFS(n, numbers, visited, 0, 0);
 	return;
