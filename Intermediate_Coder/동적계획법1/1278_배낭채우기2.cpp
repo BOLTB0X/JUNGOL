49443265cdda0b63a8b056aaebf37896b80180a9
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-21 17:48:37 +0900

    배낭 채우기2

--- Intermediate_Coder/동적계획법1/1278_배낭채우기2.cpp
+++ Intermediate_Coder/동적계획법1/1278_배낭채우기2.cpp
@@ -3,25 +3,24 @@
 
 using namespace std;
 
-//반환
-int MAX(int a, int b) {
+// 최댓값 반환
+int Max(int a, int b) {
 	return a > b ? a : b;
 }
 
 int solution(int n, int w, vector<int>& wis, vector<int>& pis) {
 	int answer = 0;
-	//dp테이블 초기화
+	// dp테이블 초기화
 	vector<vector<int>> dp(n + 1, vector<int>(w + 1, 0));
 
-	//보텀업 방식
+	// 보텀업 방식
 	for (int i = 1; i <= n; ++i) {
 		for (int j = 1; j <= w; ++j) {
-			//해당 위치는 한 행의 이전
 			dp[i][j] = dp[i - 1][j];
 
-			//제한선 이하일 경우
-			if (wis[i] <= j)
-				dp[i][j] = MAX(dp[i - 1][j - wis[i]] + pis[i], dp[i - 1][j]);
+			// 제한선 내라면
+			if (j - wis[i] >= 0) 
+				dp[i][j] = Max(dp[i - 1][j], dp[i - 1][j - wis[i]] + pis[i]);
 		}
 	}
 
@@ -31,12 +30,12 @@ int solution(int n, int w, vector<int>& wis, vector<int>& pis) {
 
 
 int main(void) {
-	//초기화
+	// 초기화
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	int n, w; //갯수, 배낭 용량
+	int n, w; // 갯수, 배낭 용량
 	int wi, pi; // 보석의 무게, 값어치
 	vector<int> wis, pis;
 
@@ -46,7 +45,6 @@ int main(void) {
 
 	for (int i = 1; i <= n; ++i) 
 		cin >> wis[i] >> pis[i];
-	
 
 	int ret = solution(n, w, wis, pis);
 	cout << ret;
