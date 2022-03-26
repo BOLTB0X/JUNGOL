dcfdc68417fa867e035b849ce0bdc1c315131066
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-26 17:08:01 +0900

    치즈
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1840&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/1840_치즈.cpp
+++ Intermediate_Coder/그래프탐색-DFS/1840_치즈.cpp
@@ -3,13 +3,11 @@
 
 using namespace std;
 
-int cnt = 0;
 vector<vector<int>> board;
-//상하우좌
 const int dy[4] = { 1,-1,0,0 };
-const int dx[4] = { 0,0,1,-1 };
+const int dx[4] = { 0,0,-1,1 };
 
-//녹을 치즈 있는지 확인
+// 녹을 수 있는 치즈가 있는 가?
 int is_Melting(int n, int m) {
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
@@ -20,57 +18,58 @@ int is_Melting(int n, int m) {
 	return 0;
 }
 
-//DFS으로 가장자리 탐색 - > 0인 지점에서 상하좌우로 탐색해서 1인걸 찾으면 바꿔주면 됌
-//치즈 - > 공기는 -1처리
+// DFS으로 가장자리 탐색 - > 0인 지점에서 상하좌우로 탐색해서 1인걸 찾으면 바꿔주면 됌
+// 치즈 - > 공기는 -1처리
+// 가장자리 체크
 void DFS(int n, int m, int y, int x) {
 	if (board[y][x] == 0) {
-		board[y][x] = -1; //방문리스트가 없으니 
+		board[y][x] = -1; // 방문리스트 대체
+
 		for (int dir = 0; dir < 4; ++dir) {
 			int ny = y + dy[dir];
 			int nx = x + dx[dir];
 
-			//범위초과
 			if (ny < 0 || nx < 0 || ny >= n || nx >= m)
 				continue;
 
-			//가장자리 치즈 발견
+			// 치즈 발견, 체크만하고 이동 X
 			if (board[ny][nx] == 1)
 				board[ny][nx] = 2;
 
-			//다시 공기 발견
+			//공기발견
 			if (board[ny][nx] == 0)
 				DFS(n, m, ny, nx);
 		}
 	}
+
 	return;
 }
 
-pair<int, int> solution(int n, int m) {
-	pair<int, int> answer(0, 0);
-	
-	//시뮬시작
-	while (is_Melting(n, m) == 1) {
-		answer.first++; //전체 라운트 카운트
-		cnt = 0;
+pair <int, int> solution(int n, int m) {
+	pair<int, int> answer = { 0,0 };
+	int cnt;
 
+	// 녹을 치즈가 없을 때까지 
+	while (is_Melting(n, m) == 1) {
+		answer.first++;
+		cnt = 0;
 		DFS(n, m, 0, 0);
+
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < m; ++j) {
-				//원상복구
+				// 원상 복구
 				if (board[i][j] == -1)
 					board[i][j] = 0;
-				//녹을 치즈
+
+				// 녹을 치즈 발견
 				if (board[i][j] == 2) {
 					cnt++;
-					//녹아질 치즈 제거
-					board[i][j] = 0;
+					board[i][j] = 0; // 공기 처리
 				}
 			}
 		}
 	}
-	
-	//바로 전 치즈 갯수 
-	answer.second = cnt;
+	answer.second = cnt; // 바로 전 치즈
 
 	return answer;
 }
