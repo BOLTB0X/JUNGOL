8b72acbfe1ef33916ac226e6e138d6de057d5541
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-29 11:00:55 +0900

    등수 찾기(ranking)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=3428&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/3428_등수 찾기(ranking).cpp
+++ Intermediate_Coder/그래프탐색-DFS/3428_등수 찾기(ranking).cpp
@@ -1,73 +1,65 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
-#define ML 100001 //최대 사이즈
+#define Max_Size 100001
 
 using namespace std;
 
 int cnt, r_cnt;
-vector<int> adj[ML]; //큰 놈들 -> 최악의 등수를 알 수 있음
-vector<int> r_adj[ML]; // 작은 놈들 -> 최고의 등수를 알 수 있음
-int visited[ML];
+vector<int> adj[Max_Size]; // 나보다 큰
+vector<int> radj[Max_Size]; // 작은
+vector<int> visited(Max_Size, 0);
 
-//기준 보다 큰 애들
-void Lower(int cur) {
-	if (visited[cur] == 1)
-		return;
-
-	visited[cur] = 1;
-	cnt++;
-
-	for (int& next : adj[cur]) 
-		Lower(next);
-	return;
-}
-
-//기준보다 작은 애들 
-void Higher(int cur) {
-	if (visited[cur] == 1)
-		return;
-
+void Higher(int n, int cur) {
 	visited[cur] = 1;
 	r_cnt++;
 
-	for (int& next : r_adj[cur]) 
-		Higher(next);
+	for (int& next : radj[cur]) {
+		if (visited[next] == 1)
+			continue;
+		Higher(n, next);
+	}
 	return;
 }
 
-pair<int, int> solution(int n, int m, int x) {
-	pair<int, int> answer;
-	cnt = 0;
-	memset(visited, 0, sizeof(int) * n + 1); //방문리스트 초기화
-	Lower(x);
-	
-	r_cnt = 0;
-	memset(visited, 0, sizeof(int) * n + 1); //방문리스트 초기화
-	Higher(x);
+void Lower(int n, int cur) {
+	visited[cur] = 1;
+	cnt++;
 
-	answer.first = r_cnt;
-	answer.second = n - cnt + 1;
+	for (int& next : adj[cur]) {
+		if (visited[next] == 1)
+			continue;
+		Lower(n, next);
+	}
+	return;
+}
+
+vector<int> solution(int n, int m, int x) {
+	vector<int> answer(2, 0);
+	cnt = 0, r_cnt = 0;
+
+	Higher(n, x);
+	visited = vector<int>(Max_Size, 0);
+	Lower(n, x);
+
+	answer[0] = r_cnt;
+	answer[1] = n - cnt + 1;
+
 	return answer;
 }
 
 int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
 	int n, m, x;
-	cin >> n >> m >> x;
+	int a, b;
 
+	cin >> n >> m >> x;
 	for (int i = 0; i < m; ++i) {
-		int a, b;
 		cin >> a >> b;
+
 		adj[a].push_back(b);
-		r_adj[b].push_back(a);
+		radj[b].push_back(a);
 	}
 
-	pair<int, int> ret = solution(n, m, x);
-	cout << ret.first << ' ' << ret.second;
+	vector<int> ret = solution(n, m, x);
+	cout << ret[0] << ' ' << ret[1];
 	return 0;
 }
