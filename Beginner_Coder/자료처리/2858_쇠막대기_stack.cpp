f36b5d91fb025e8d3411ec7981216ff9ff1228b4
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-31 15:33:54 +0900

    쇠막대기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=2858&sca=2070

--- Beginner_Coder/자료처리/2858_쇠막대기_stack.cpp
+++ Beginner_Coder/자료처리/2858_쇠막대기_stack.cpp
@@ -8,18 +8,17 @@ int solution(string stick) {
 	int answer = 0; 
 	int size = stick.length();
 
-	stack<int> st; //연산을 위한 스택 선언
-
+	stack<int> st; // 스택 선언
 	for (int i = 0; i < size; ++i) {
-		if (stick[i] == '(') 
-			st.push(1); // 스택 삽입
-		// ')'을 만나는 경우
+		// 오픈 이면
+		if (stick[i] == '(')
+			st.push(1);
+
+		// 클로즈가 나오면
 		else {
-			st.pop(); 
-			//바로전이 오픈 기호라면
-			if (stick[i - 1] == '(') 
-				answer += (st.size());
-			//바로 전이 클로즈라면
+			st.pop(); // pop
+			if (stick[i - 1] == '(')
+				answer += st.size();
 			else
 				answer++;
 		}
@@ -29,16 +28,10 @@ int solution(string stick) {
 }
 
 int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
 	string stick;
 	cin >> stick;
 
 	int ret = solution(stick);
-
 	cout << ret;
 	return 0;
 }
