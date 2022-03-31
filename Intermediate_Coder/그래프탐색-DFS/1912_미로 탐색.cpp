47b20bcc0289e3b8649b21294c44e8f449d77e20
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-31 14:27:45 +0900

    미로 탐색
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1912&sca=3090

--- Intermediate_Coder/그래프탐색-DFS/1912_미로 탐색.cpp
+++ Intermediate_Coder/그래프탐색-DFS/1912_미로 탐색.cpp
@@ -1,58 +1,59 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define MS 100001 #최대 길이
+#define Max_Size 100001 // 최대 길이
 
 using namespace std;
 
-vector<int> adj[MS];
+vector<int> adj[Max_Size]; // 인접리스트
 
-//깊이우선탐색 
-void DFS(int n, vector<int>& visited, int cur, vector<int>& path) {
-	path.push_back(cur);
-	visited[cur] = 1; //재방문
+// 깊이우선탐색
+void DFS(int n, vector<int>& visited, vector<int>& path, int cur) {
+	visited[cur] = 1;
+	path.push_back(cur); // 삽입
 
+	// 오름차순으로 다음 노드들이 정렬되어 있으므로
 	for (int& next : adj[cur]) {
-		//재방문 
+		// 재방문
 		if (visited[next] == 1)
 			continue;
 
-		//다시 호출
-		DFS(n, visited, next, path);
+		// 호출
+		DFS(n, visited, path, next);
 	}
-	return;
 
+	return;
 }
 
-void solution(int n, int m) {
+vector<int> solution(int n, int m) {
 	vector<int> answer;
-	vector<int> visited(n + 1, 0);
 
-	//정렬 - > 작은 수부터 진행하는 게
+	vector<int> visited(n + 1, 0); // 방문리스트
+	
+	// 한 번도 들르지 않은 방이 있다면 그 중 가장 번호가 작은 방으로 가고
+	// 정렬해줘야함
 	for (int i = 1; i <= n; ++i)
 		sort(adj[i].begin(), adj[i].end());
 
-	//깊이우선 탐색으로 탐색 시작
-	DFS(n, visited, 1, answer);
+	DFS(n, visited, answer, 1);
 
-	for (int& a : answer)
-		cout << a << ' ';
-
-	return;
+	return answer;
 }
 
 int main(void) {
-	int n, m;
-	int a, b;
-	cin >> n >> m;
+	int n, m, a, b;
 
+	cin >> n >> m;
 	for (int i = 0; i < m; ++i) {
 		cin >> a >> b;
+
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
 
-	solution(n, m);
+	vector<int> ret = solution(n, m);
+	for (int& r : ret)
+		cout << r << ' ';
 
 	return 0;
 }
