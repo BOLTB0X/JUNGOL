87010bd75248541539660d0668bd82a47ef8faa8
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-01 14:59:04 +0900

    최대공약수, 최소공배수
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1002&sca=2030

--- Beginner_Coder/수학1/1002_최대공약수, 최소공배수.c
+++ Beginner_Coder/수학1/1002_최대공약수, 최소공배수.c
@@ -1,17 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//최대공약수
+typedef struct {
+	int answer1, answer2;
+} Pair;
+
+// 최대공약수
 int gcd(int a, int b) {
-	if (a % b == 0)
-		return b;
-	else
-		return gcd(b, a % b);
+	while (b != 0) {
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+
+	return a;
 }
 
-//최소공배수
+// 최소공배수
 int lcm(int a, int b) {
 	return a * b / gcd(a, b);
+}
+
+Pair solution(int n, int* arr) {
+	Pair answer = { 0,0 };
+
+	answer.answer1 = arr[0];
+	answer.answer2 = arr[0];
+
+	for (int i = 1; i < n; ++i) {
+		answer.answer1 = gcd(answer.answer1, arr[i]);
+		answer.answer2 = lcm(answer.answer2, arr[i]);
+	}
+
+	return answer;
 }
 
 int main(void) {
@@ -22,13 +43,8 @@ int main(void) {
 	for (int i = 0; i < n; ++i)
 		scanf("%d", &arr[i]);
 
-	int ret1 = arr[0];
-	int ret2 = arr[0];
-
-	for (int i = 1; i < n; ++i) {
-		ret1 = gcd(ret1, arr[i]);
-		ret2 = lcm(ret2, arr[i]);
-	}
-	printf("%d %d\n", ret1, ret2);
+	Pair ret = solution(n, arr);
+	printf("%d ",ret.answer1);
+	printf("%d",ret.answer2);
 	return 0;
 }
