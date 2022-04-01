db385e970ffe5e03917506ebb389899d9c536a3f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-01 14:51:45 +0900

    최대공약수와최소공배수
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1658&sca=2030

--- Beginner_Coder/수학1/1658_최대공약수와최소공배수.c
+++ Beginner_Coder/수학1/1658_최대공약수와최소공배수.c
@@ -1,27 +1,40 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
-//최대공약수
-int gcd(int a, int b) {
-	if (a % b == 0)
-		return b;
-	else
-		return gcd(b, a % b);
+// 스와핑
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+	return;
 }
 
-//최소공배수
+// 최대공약수
+int gcd(int a, int b) {
+	if (a < b)
+		swap(&a, &b);
+
+	while (b != 0) {
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+
+	return a;
+}
+
+// 최소공배수
 int lcm(int a, int b) {
 	return a * b / gcd(a, b);
 }
 
 int main(void) {
-	int n, m;
-	scanf("%d %d", &n, &m);
+	int number1, number2;
 
-	int ret1 = gcd(n, m);
-	int ret2 = lcm(n, m);
+	scanf("%d %d", &number1, &number2);
+	
+	printf("%d\n", gcd(number1, number2));
+	printf("%d\n", lcm(number1, number2));
 
-	printf("%d\n", ret1);
-	printf("%d\n", ret2);
 	return 0;
 }
