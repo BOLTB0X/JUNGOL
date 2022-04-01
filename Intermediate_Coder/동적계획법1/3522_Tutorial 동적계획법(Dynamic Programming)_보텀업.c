6709b4d2318678e055b2fcfa548140f7b58e99bd
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-01 15:08:10 +0900

    Tutorial 동적계획법(Dynamic Programming)
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=3522&sca=3060

--- Intermediate_Coder/동적계획법1/3522_Tutorial 동적계획법(Dynamic Programming)_보텀업.c
+++ Intermediate_Coder/동적계획법1/3522_Tutorial 동적계획법(Dynamic Programming)_보텀업.c
@@ -1,19 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int dp[100001] = { 0, };
+// ë³´í…€ì—… -> ë°˜ë³µë¬¸
+int do_dynamic(int n) {
+	int* dp = malloc(sizeof(int) * n);
+	dp[0] = dp[1] = 1;
+
+	for (int i = 2; i < n; ++i)
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+
+	int ret = dp[n - 1];
+	free(dp);
+	return ret;
+}
 
 int main(void) {
 	int n;
 	scanf("%d", &n);
-	dp[1] = 1;
-	dp[2] = 1;
 
-	//º¸ÅÓ¾÷
-	for (int i = 3; i <= n; ++i)
-		dp[i] = ((dp[i - 1] + dp[i - 2]) % 1000000007);
-
-	printf("%d", dp[n]);
-
+	int ret = do_dynamic(n);
+	printf("%d", ret);
 	return 0;
 }
