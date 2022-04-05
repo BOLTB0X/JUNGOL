ad93f30ad0bb4c01e6088e5c9f3360eab62018ae
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-05 15:39:07 +0900

    계단 오르기
    http://jungol.co.kr/bbs/board.php?bo_table=pbank&code=1520&sca=3060

--- Intermediate_Coder/동적계획법1/1520_계단 오르기.cpp
+++ Intermediate_Coder/동적계획법1/1520_계단 오르기.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int steps[301];
-int dp[301] = { 0, };
+vector<int> dp;
+vector<int> steps;
 
 //최댓값 반환
 int Max(int a, int b) {
 	return a > b ? a : b;
 }
 
+int solution(int n) {
+	int answer = 0;
+	dp.resize(n, 0);
+
+	dp[0] = steps[0];
+	dp[1] = steps[0] + steps[1];
+	dp[2] = Max(steps[1] + steps[2], steps[0] + steps[2]);
+
+	for (int i = 3; i < n; ++i) 
+		dp[i] = steps[i] + Max(dp[i - 2], dp[i - 3] + steps[i - 1]);
+	
+	answer = dp[n - 1];
+	return answer;
+}
+
 int main(void) {
 	int n;
-
 	cin >> n;
-	for (int i = 1; i <= n; ++i) 
+	steps.resize(n, 0);
+
+	for (int i = 0; i < n; ++i)
 		cin >> steps[i];
 
-	dp[1] = steps[1];
-	dp[2] = steps[1] + steps[2];
-	dp[3] = Max(steps[1] + steps[3], steps[2] + steps[3]);
-
-	//보텀업 
-	for (int i = 4; i <= n; ++i) 
-		dp[i] = steps[i] + Max(dp[i - 3] + steps[i - 1], dp[i - 2]);
-	
-	cout << dp[n];
+	int ret = solution(n);
+	cout << ret;
 	return 0;
 }
